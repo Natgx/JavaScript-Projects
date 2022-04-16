e3c93918959849be34eea7c5ed1d5b00d531b86a
Natgx <102414039+Natgx@users.noreply.github.com>
2022-04-16 16:51:52 +0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT
    Pushed my Basic JavaScript Projects folder with all the requirement of the assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("Hello, World!"); // Creatng a alert box that displays Hello, World!
+document.write("Hi, World!"); // Display the string "Hi, World!"
+var n = "n stands for \"Noel\"."; // Using \"" or \'
+document.write(n); // Display the value of the variable n
+var f = "Hi, Friend!"; //Assigning a value to the variable f
+window.alert(f); // Display the value of f in the alert box
+document.write("Hi my full name is" + " Nathaniel G. Noel");
+var g = "Goodmorning" + " Sir!"; // Concatenating 2 strings
+document.write(g); // Display the value of g
+var a = "Apple", b = "Boy", c = "Carrot"; //Assigning variables all in one statement
+document.write(c.fontcolor("green")); //Displaying the value of the variable c and making it have a font color green
+var e = 5 + 5; // Assigning the var e to an expression
+document.write(e); // Displaying the value of the variable e
